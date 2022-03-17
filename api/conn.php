588c8a7d2ef2914c858af2d3ea6ee2ee2b2deb50
Francis Ong <francis.ong.pixel8@gmail.com>
2022-03-17 17:19:28 +0800

    #98 -> modify database name -> new value (todo)

--- api/conn.php
+++ api/conn.php
@@ -1,4 +1,4 @@
 <?php
   //Database Init -> Config (hostname, username, password, database name)
-  $conn = new MysqliDb ('localhost', 'root', '', 'todo_db');
+  $conn = new MysqliDb ('localhost', 'root', '', 'todo');
 ?>
