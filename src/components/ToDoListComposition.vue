1ef1ec7ad768ae6139ccbaacbbee2e1bc343b060
Francis Ong <francis.ong.pixel8@gmail.com>
2022-03-17 10:49:35 +0800

    #98 -> import relocated setup composition for todolist.js, add sample content to todolist and integrate functions addItem, deleteItem (JS only)

--- src/components/ToDoListComposition.vue
+++ src/components/ToDoListComposition.vue
@@ -1,38 +1,49 @@
 <template>
   <!-- To-Do List Container -->
   <div class="container">
-    <h1 class="text-center">To-Do List App</h1>
+    <h1 class="text-center my-5">To-Do List App</h1>
     <!-- To-Do List Add Form -->
-    <form>
-      <div class="form-group row">
-        <div class="col-5">
-          <input type="text" class="form-control" />
+    <div class="d-flex justify-content-center">
+      <form @submit.prevent="addItem(toDoItem)">
+        <div class="form-group row">
+          <div class="col-10">
+            <input type="text" class="form-control" v-model="toDoItem" />
+          </div>
+          <div class="col-2">
+            <button type="submit" class="btn btn-dark">Add</button>
+          </div>
         </div>
-        <div class="col-5">
-          <button type="submit" class="btn btn-dark col-md-4">Add Item</button>
-        </div>
-        <div class="col-2">
-          <select>
-            <option value="" disabled selected>Status</option>
-            <option value="P">Pending</option>
-            <option value="IP">In-progress</option>
-            <option value="D">Done</option>
-          </select>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
     <!-- ** End To-Do List Add Form ** -->
     <!-- To-Do List Table -->
-    <div>
+    <div class="mt-5">
       <table class="table table-striped" id="toDoTable">
         <thead>
           <tr>
+            <th>#</th>
             <th>My To-Do List</th>
             <th>Item Status</th>
             <th>Action</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          <tr v-for="(value, index) in TO_DO_LIST" :key="index">
+            <td>{{ index + 1 }}</td>
+            <td>{{ value.item }}</td>
+            <td>
+              <span class="badge" :class="getBadgeClass(value.status)">
+                {{ value.status }}
+              </span>
+            </td>
+            <td>
+              <button class="btn btn-primary">Edit</button>
+              <button @click="deleteItem(index)" class="btn btn-danger">
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
     <!-- ** End To-Do List Table ** -->
@@ -47,21 +58,33 @@ import "datatables.net-dt/css/jquery.dataTables.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import $ from "jquery";
-import { onMounted } from "vue";
-//import axios from "axios";
+import { ref, onMounted } from "vue";
+import {
+  TO_DO_LIST,
+  addItem,
+  getBadgeClass,
+  deleteItem,
+} from "@/composables/ToDoList.js";
 
 export default {
+  name: "TodoListComposition",
   setup() {
-    //let toDoList = [];
-    //let addItem = ref(null);
-    //let itemStatus = ref(null);
+    let toDoItem = ref(null);
+    // ===============================HOOKS==========================================
     /**
      * This Lifecyclehook will generate an instance of dataTable for our ToDoList Table
-     * For Search & Pagings Functionality
+     * For Search & Pagings Functionality after the DOM is loaded
      */
     onMounted(() => {
       $("#toDoTable").DataTable();
     });
+    return {
+      TO_DO_LIST,
+      toDoItem,
+      addItem,
+      getBadgeClass,
+      deleteItem,
+    };
   },
 };
 </script>
