7ba5f408387cb8f46f234759122fa7a45e32ec26
Francis Ong <francis.ong.pixel8@gmail.com>
2022-03-17 15:35:39 +0800

    #98 -> add axios get for fetching sample data through the api (using action retrieve)

--- src/composables/ToDoList.js
+++ src/composables/ToDoList.js
@@ -1,14 +1,29 @@
 import { ref } from "vue";
-//import axios from "axios";
+import axios from "axios";
 
 // ===============================INITIALIZATION==========================================
-const TO_DO_LIST = ref([
-  {
-    item: "Codes",
-    status: "Pending",
-  },
-]);
+const TO_DO_LIST = ref([]);
+const API_URL = ref("http://localhost/ojt-training-repo/api/action.php");
 // ===============================FUNCTIONS==========================================
+/**
+ * This function will retrieve all of the data in the database (item, status, date_created)
+ * if the action specified is not include in the following
+ * ('retrieve', 'update', 'delete', 'insert')
+ * it will alert the user -> "Invalid Action Request" (callback == 1)
+ * else proceed to the action specified (e.g retrieve -> retrieve data)
+ * retrieveList function have self invoke behavior
+ */
+(function retrieveList() {
+  axios.get(`${API_URL.value}?action=retrieve`).then(function (response) {
+    if (response.data == 1) {
+      alert("Invalid Action Request");
+    } else {
+      //TO_DO_LIST.value.splice(0, TO_DO_LIST.value.length); // delete all items -> array
+      //TO_DO_LIST.value.push(...response.data); // put all the data objects -> array
+      console.log(response.data);
+    }
+  });
+})();
 /**
  * This function adds an item in the array -> TO_DO_LIST []
  * status -> "PENDING" by default
@@ -26,7 +41,9 @@ let addItem = (item) => {
  *@param index -> position of object in toDoList array
  */
 let deleteItem = (index) => {
-  TO_DO_LIST.value.splice(index, 1);
+  if (confirm("Are you sure you want to delete this item ?")) {
+    TO_DO_LIST.value.splice(index, 1);
+  }
 };
 /**
  * This getBadgeClass function will render the bg class from bootstrap class
