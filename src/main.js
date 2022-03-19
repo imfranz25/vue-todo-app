2285148b88ad8667d4d3b2853fb4373b7678d550
Francis Ong <francis.ong.pixel8@gmail.com>
2022-03-19 12:07:49 +0800

    #98 -> relocate import ads-on (e.g bootstrap and among others)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+//ads-on
+import "jquery/src/jquery.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "popper.js/dist/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "datatables.net-dt/js/dataTables.dataTables";
+import "datatables.net-dt/css/jquery.dataTables.min.css";
 
 createApp(App).mount("#app");
